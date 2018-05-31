5d6113c461dbccc69cef944967fda2a7581bd982
woaij007 <Cola@juns-mac-mini.lan>
2018-05-30 18:21:38 -0700

    init commit

---
+++ src/features/player/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from "react-redux";
+import walkSprite from './player_walk.png';
+
+function Player(props) {
+  return(
+    <div
+      style={{
+        position: 'relative',
+        top: props.position[1],
+        left: props.position[0],
+        backgroundImage: `url(${walkSprite})`,
+        backgroundPosition: '0 0',
+        width: '40px',
+        height: '40px',
+      }}
+    />
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    ...state.player,
+  }
+}
+
+export default connect(mapStateToProps)(Player);
